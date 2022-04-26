b54ad0119c608aee8c3c4f2a4ac51856fb6b9629
Pratyush Raj <praty2k1@gmail.com>
2022-04-27 01:39:46 +0530

    added fab

---
+++ frontend/src/components/Fab.js
@@ -0,0 +1,41 @@
+import Button from '@mui/material/Button';
+import { PlayArrow, StopRounded } from "@mui/icons-material";
+import { createTheme,Stylesheet } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+
+const theme = createTheme();
+
+theme.spacing(2);
+export default function Faab() {
+    return (
+        <>
+        <Button
+    style={{
+        borderRadius: 15,
+        backgroundColor: "#4a126b",
+        padding: "10px 25px",
+        alignItems: "center",
+        fontSize: "14px"
+    }}
+    variant="contained"
+    >
+    <PlayArrow sx={{ mr: 0.5 }} />
+    RUN
+</Button>
+
+<Button
+    style={{
+        borderRadius: 15,
+        backgroundColor: "#1c2566",
+        padding: "10px 25px",
+        fontSize: "14px"
+    }}
+    variant="contained"
+    >
+    <StopRounded sx={{ mr: 0.5 }} />
+    STOP
+</Button>
+
+</>
+    )
+};
